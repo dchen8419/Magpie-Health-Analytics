d9bdda3364a4439a438dff36d9f259ae8c7766a9
David Chen <DavidChen@Davids-iMac.local>
2023-09-06 13:07:11 -0400

    added pivote table and gpa

--- grade_calculator.py
+++ grade_calculator.py
@@ -4,21 +4,28 @@ import pandas as pd
 final_student_grades = pd.DataFrame()
 df = pd.read_csv('student_grades.csv')
 # Combine "FIRST" and "LAST" columns into a single "FULL NAME" column
-df['FULL NAME'] = df['FIRST'] + ' ' + df['LAST']
+df['FULL_NAME'] = df['FIRST'] + ' ' + df['LAST']
 
 # Drop the "FIRST" and "LAST" columns if you no longer need them
 df = df.drop(['FIRST', 'LAST'], axis=1)
 
 # Reorder the columns with the last column as the first column
-df = df[['FULL NAME'] + [col for col in df.columns if col != 'FULL NAME']]
+df = df[['FULL_NAME'] + [col for col in df.columns if col != 'FULL_NAME']]
 
-# Create a new column with calculated values
+# Create a new column with class average
 df['CLASS_AVERAGE'] = df[['EXAM_1_GRADE','EXAM_2_GRADE','EXAM_3_GRADE','EXAM_4_GRADE']].mean(axis=1)
 
+#Create a new column to calculate GPA
+df['GPA'] = df[['FULL_NAME', 'CLASS_AVERAGE']].groupby(['FULL_NAME']).transform('mean')
+# print(df)
 
+#Create filter out the common names and create the classes are columns with the values in the rows
+df.pivot_table(index='FULL_NAME', columns='CLASS', values='CLASS_AVERAGE')
+
+print(df)
 
 # Save the updated DataFrame to a new CSV file
 df.to_csv('final_student_grades.csv', index=False)
-# print(df)
+# print(df.pivot_table(index='FULL NAME', columns='CLASS', values='CLASS_AVERAGE'))
 
 
